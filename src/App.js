9fb0069e956734ba1562c61851d2b389e737d26b
mahmud886 <iqbal886mahmud@gmail.com>
2020-03-23 19:48:17 +0600

    this commit uploading all the data to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Route, Switch, BrowserRouter} from 'react-router-dom';
+import {AuthProvider, PrivateRoute} from './Components/SignUp/useAuth';
 import './App.css';
+import Header from './Components/Header/Header';
+import Banner from './Components/Banner/Banner';
+import Foods from './Components/Foods/Foods';
+import Footer from './Components/Footer/Footer';
+import Features from './Components/Features/Features';
+import FoodDetails from './Components/FoodDetails/FoodDetails';
+import SignUp from './Components/SignUp/SignUp';
+import Shipment from './Components/Shipment/Shipment';
+import OrderComplete from './Components/OrderComplete/OrderComplete';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [cart, setCart] = useState([]);
+    const [deliveryDetails, setDeliveryDetails] = useState(
+        {todoor: null, road: null, flat: null, businessname: null, address: null}
+    );
+    const deliveryDetailsHandler = (data) => {
+        setDeliveryDetails(data)
+    }
+    const clearCart = () => {
+        setCart([])
+    }
+    const cartHandler = (data) => {
+        const alreadyAdded = cart.find(crt => crt.id == data.id);
+        const newCart = [
+            ...cart,
+            data
+        ]
+        setCart(newCart);
+        if (alreadyAdded) {
+            const reamingCarts = cart.filter(crt => cart.id != data);
+            setCart(reamingCarts);
+        } else {
+            const newCart = [
+                ...cart,
+                data
+            ]
+            setCart(newCart);
+        }
+
+    }
+
+    return (
+        <AuthProvider>
+            <Router>
+                <div className="main">
+                    <Switch>
+                        <Route exact="exact" path="/">
+                            <Header cart={cart}></Header>
+                            <Banner></Banner>
+                            <Foods cart={cart}></Foods>
+                            <Features></Features>
+                            <Footer></Footer>
+                        </Route>
+                        <Route path="/food/:id">
+                            <Header cart={cart}></Header>
+                            <FoodDetails cart={cart} cartHandler={cartHandler}></FoodDetails>
+                            <Footer></Footer>
+                        </Route>
+                        <PrivateRoute path="/checkout">
+                            <Header cart={cart}></Header>
+                            <Shipment
+                                deliveryDetails={deliveryDetails}
+                                deliveryDetailsHandler={deliveryDetailsHandler}
+                                cart={cart}
+                                clearCart={clearCart}></Shipment>
+                            <Footer></Footer>
+                        </PrivateRoute>
+                        <PrivateRoute path="/order-complete">
+                            <Header cart={cart}></Header>
+                            <OrderComplete deliveryDetails={deliveryDetails}></OrderComplete>
+                            <Footer></Footer>
+                        </PrivateRoute>
+                        <Route path="/login">
+                            <SignUp></SignUp>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </AuthProvider>
+    );
 }
 
 export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBRwbkenW-ezNPSiaGUtMf030PHc8ohIV0",
+    authDomain: "hot-onion-restaurent-b915d.firebaseapp.com",
+    databaseURL: "https://hot-onion-restaurent-b915d.firebaseio.com",
+    projectId: "hot-onion-restaurent-b915d",
+    storageBucket: "hot-onion-restaurent-b915d.appspot.com",
+    messagingSenderId: "288490797002",
+    appId: "1:288490797002:web:088a3dfe5380bee8320133"
+};
+
+export default firebaseConfig;
